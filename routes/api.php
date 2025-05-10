e8f6509683f9f00d86bee4efb1f171d7cf0df76d
Kavindu Methmal <kavindumethmal1234gmail.com>
2025-05-10 20:50:13 +0530

    Create user sign-in endpoint.

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::post('/sign-in', function (Request $request) {
     return 'hello';
 });
