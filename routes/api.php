51b37c9dc64d580f31bef9563223fe27e67e30e4
Kavindu Methmal <kavindumethmal1234gmail.com>
2025-05-11 13:09:07 +0530

    Create user sign-in validation part.

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/sign-in', function (Request $request) {
-    return 'hello';
-});
+Route::post('/sign-in' ,[UserController::class,'userSighIn']);
+
